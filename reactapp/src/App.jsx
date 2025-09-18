2bf3afec4c34670f80dd2a406d5920dd0ba85b12
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-18 09:28:27 +0200

    Empty cart

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -29,13 +29,13 @@ state = {
           <label htmlFor="">Ár:</label>
           <input type="number" id="input_nev" name="input_nev" value={0} onChange={value=>this.setState({nev: value})}/> <br />
           <label htmlFor="">Mennyiség:</label>
-          <input type="number" id="input_nev" name="input_nev" placeholder="Darab..."/> <br />
+          <input type="number" id="input_nev" name="input_nev" placeholder="Darab..." onChange={value=>this.setState({nev: value})}/> <br />
           <input type="button" name="btn_submit" id="btn_submit" value="Hozzáadás a kosárhoz" onClick={this.handleButtonClick}/> <br />
         </form>
         <hr />
         <h3>A kosár tartalma</h3>
           {
-            this.state.cart.items.length >= 1 && 
+            this.state.cart.items.length >= 1 ?
             <ul>
               {
                 this.state.cart.items.map((item, idx) => <li key={idx}>
@@ -43,6 +43,8 @@ state = {
                 </li>)
               }
             </ul>
+            :
+              <p>Üres a kosár!</p>
           }
       </div>
     )
