536a26971a8c6b418905eaf7e12b414d96551652
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-18 09:55:06 +0200

    Perfection

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -9,7 +9,7 @@ state = {
         ]
       },
     nev: null,
-    ar: null,
+    ar: 0,
     db: null,
  }
 
@@ -17,6 +17,18 @@ state = {
   e.preventDefault();
 
   //TODO - implement cart item insert
+  console.log("handleButtonClick state", this.state)
+  if(this.state.ar && this.state.ar>0 && this.state.db && this.state.db>0 && this.state.nev) {
+    this.setState({cart: {items: 
+      [...this.state.cart.items,
+        {name: this.state.nev,
+         price: +this.state.ar,
+         quantity: +this.state.db
+      }]
+    }})
+  } else {
+    alert("Please fill all fields")
+  }
  }
 
   render() {
@@ -25,11 +37,11 @@ state = {
         <form>
           <h1>Termék hozzáadása a kosárhoz</h1>
           <label htmlFor="">Termék neve:</label>
-          <input type="text" id="input_nev" name="input_nev" placeholder="Név..." onChange={value=>this.setState({nev: value})}/> <br />
+          <input type="text" id="input_nev" name="input_nev" placeholder="Név..." onChange={e=>this.setState({nev: e.target.value})} required/> <br />
           <label htmlFor="">Ár:</label>
-          <input type="number" id="input_nev" name="input_nev" value={0} onChange={value=>this.setState({nev: value})}/> <br />
+          <input type="number" id="input_ar" name="input_ar" value={this.state.ar} onChange={e=>this.setState({ar: e.target.value})}/> <br />
           <label htmlFor="">Mennyiség:</label>
-          <input type="number" id="input_nev" name="input_nev" placeholder="Darab..." onChange={value=>this.setState({nev: value})}/> <br />
+          <input type="number" id="input_db" name="input_db" placeholder="Darab..." onChange={e=>this.setState({db: e.target.value})} required/> <br />
           <input type="button" name="btn_submit" id="btn_submit" value="Hozzáadás a kosárhoz" onClick={this.handleButtonClick}/> <br />
         </form>
         <hr />
